3ae4d343a8afeb29aa638278cf0e45fdfacf7bfe
italotelly <81748009+italotelly@users.noreply.github.com>
2025-04-01 00:29:24 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// 수학은 체육과목 입니다
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    long n;
+    cin >> n;
+
+    cout << 4 * n;
+
+    return 0; // 정상종료
+}
