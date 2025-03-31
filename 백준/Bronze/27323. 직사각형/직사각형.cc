b84453b407305e047dbec8282370cf9c6855c25b
italotelly <81748009+italotelly@users.noreply.github.com>
2025-04-01 00:29:34 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// 직사각형
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int A, B;
+
+    cin >> A >> B;
+
+    cout << A * B;
+
+    return 0; // 정상종료
+}
