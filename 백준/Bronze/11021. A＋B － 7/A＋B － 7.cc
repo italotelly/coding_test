b1d5702ba5db4dc9607d4e58994f3be3c61e7d67
italotelly <81748009+italotelly@users.noreply.github.com>
2025-04-01 00:32:09 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+// A + B - 7
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int T;
+    cin >> T;
+
+    int A, B;
+
+    for (int i = 0; i < T; i++)
+    {
+        cin >> A >> B;
+        if (A < 0 || A > 10 || B < 0 || B > 10)
+        {
+            return 1; // 비정상 종료
+        }
+        cout << "Case #" << i + 1 << ": " << A + B << "\n";
+    }
+
+    return 0; // 정상종료
+}
