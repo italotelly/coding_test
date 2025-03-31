310516007a76279b3ca9d6c737244b4008bda8af
italotelly <81748009+italotelly@users.noreply.github.com>
2025-04-01 00:40:58 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+// 단어의 개수
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    cout << "         ,r\'\"7" << "\n";
+    cout << "r`-_   ,'  ,/" << "\n";
+    cout << " \\. \". L_r\'" << "\n";
+    cout << "   `~\\/" << "\n";
+    cout << "      |" << "\n";
+    cout << "      |" << "\n";
+    return 0; // 정상종료
+}
