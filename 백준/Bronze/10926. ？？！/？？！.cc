2279378bc22c3e91162ca29c26ae325a7238b028
italotelly <81748009+italotelly@users.noreply.github.com>
2025-04-01 00:33:10 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    string A;
+    cin >> A;
+
+    if(A == "joonas" || "baekjoon")
+        cout << A << "?\?!";
+    
+    return 0;
+}
