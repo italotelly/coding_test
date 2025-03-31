895fdd56e5790779d130f15172feb51d213b6477
italotelly <81748009+italotelly@users.noreply.github.com>
2025-04-01 00:29:08 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// 알고리즘 수업 - 알고리즘의 수행 시간 1
+
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    cout << "1\n0";
+    return 0; // 정상종료
+}
