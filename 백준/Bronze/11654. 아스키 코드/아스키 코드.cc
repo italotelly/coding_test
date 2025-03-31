a8ebd8882d50a2bca5fa77bc7709054ad45d3195
italotelly <81748009+italotelly@users.noreply.github.com>
2025-04-01 00:31:13 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// 아스키 코드
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    char S;
+
+    cin >> S;
+
+    cout << int(S);
+
+    return 0; // 정상종료
+}
