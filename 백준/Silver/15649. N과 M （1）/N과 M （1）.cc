38f94290451d50542ed583d453c0abd722fe4147
italotelly <81748009+italotelly@users.noreply.github.com>
2025-03-31 23:39:08 +0900

    [Silver III] Title: N과 M (1), Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#define MAX 8
+
+using namespace std;
+
+int arr[MAX];
+bool visited[MAX];
+vector<int> vec;
+int cnt, n, m;
+
+void Print()
+{
+    for (int i = 0; i < vec.size(); i++)
+        cout << vec[i] << " ";
+    cout << '\n';
+    return;
+}
+
+void DFS(int cnt)
+{
+    if (cnt == m)
+    {
+        Print();
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i])
+            continue;
+        visited[i] = true;
+        vec.push_back(arr[i]);
+        DFS(cnt + 1);
+        vec.pop_back();
+        visited[i] = false;
+    }
+}
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = i + 1;
+        visited[i] = false;
+    }
+
+    DFS(0);
+
+    return 0;
+}
