29a5b8971a7d3c86afd42b1f6b6f1e84c2362e39
italotelly <81748009+italotelly@users.noreply.github.com>
2025-05-09 00:04:43 +0900

    [D3] Title: 색상환, Time: 4 ms, Memory: 372 KB -BaekjoonHub

---
+++ SWEA/D3/23003. 색상환/색상환.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string ary[6] = {"red", "orange", "yellow", "green", "blue", "purple"};
+
+// »ö»óÈ¯
+int main()
+{
+    int T;
+
+    cin >> T;
+
+    string s, t;
+
+    for (int i = 0; i < T; i++)
+    {
+        cin >> s >> t;
+
+        if (s == t)
+        {
+            cout << "E\n";
+        }
+        else
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                if (s == ary[j])
+                {
+                    int a = j + 1;
+                    int b = j - 1;
+                    int c = j + 3;
+                    if (a >= 6)
+                    {
+                        a = 0;
+                    }
+                    if (b < 0)
+                    {
+                        b = 5;
+                    }
+                    if (c >= 6)
+                    {
+                        c -= 6;
+                    }
+                    if (ary[a] == t)
+                    {
+                        cout << "A\n";
+                    }
+                    else if (ary[b] == t)
+                    {
+                        cout << "A\n";
+                    }
+                    else if (ary[c] == t)
+                    {
+                        cout << "C\n";
+                    }
+                    else
+                    {
+                        cout << "X\n";
+                    }
+                    break;
+                }
+            }
+        }
+    }
+    return 0;
+}
