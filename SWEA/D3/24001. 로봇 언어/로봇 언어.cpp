b09c44375eba329bf44eb7cc55e640ed3c9c0caf
italotelly <81748009+italotelly@users.noreply.github.com>
2025-05-10 18:56:09 +0900

    [D3] Title: 로봇 언어, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D3/24001. 로봇 언어/로봇 언어.cpp
+++ SWEA/D3/24001. 로봇 언어/로봇 언어.cpp
@@ -4,45 +4,37 @@
 #include <algorithm>
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int T;
     cin >> T;
 
-    while (T--)
-    {
+    while (T--) {
         string S;
         cin >> S;
 
-        int pos1 = 0, maxDist1 = 0; // case 1: '?' 전부 R
-        int pos2 = 0, maxDist2 = 0; // case 2: '?' 전부 L
+        int pos1 = 0, maxDist1 = 0; // '?'를 모두 R로 처리
+        int pos2 = 0, maxDist2 = 0; // '?'를 모두 L로 처리
 
-        for (char c : S)
-        {
-            if (c == 'L')
-                pos1--, pos2--;
-            else if (c == 'R')
-                pos1++, pos2++;
-            else
-            {
-                // '?'를 R로 간주한 경우
-                pos1++;
-                maxDist1 = max(maxDist1, abs(pos1));
-
-                // '?'를 L로 간주한 경우
+        for (char c : S) {
+            if (c == 'L') {
+                pos1--;
                 pos2--;
-                maxDist2 = max(maxDist2, abs(pos2));
-                continue;
+            } else if (c == 'R') {
+                pos1++;
+                pos2++;
+            } else { // '?'
+                pos1++;  // R로 간주
+                pos2--;  // L로 간주
             }
 
+            // 공통: 이동 후 현재까지의 최대 거리 갱신
             maxDist1 = max(maxDist1, abs(pos1));
             maxDist2 = max(maxDist2, abs(pos2));
         }
 
-        // 최댓값 출력
         cout << max(maxDist1, maxDist2) << '\n';
     }
 
