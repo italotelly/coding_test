415e4130442476d796a35d05fd536120eb684f8c
italotelly <81748009+italotelly@users.noreply.github.com>
2025-05-20 00:05:13 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문1, Time: 4 ms, Memory: 396 KB -BaekjoonHub

--- SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.cpp
+++ SWEA/D3/1215. ［S／W 문제해결 기본］ 3일차 － 회문1/［S／W 문제해결 기본］ 3일차 － 회문1.cpp
@@ -1,51 +1,69 @@
 #include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
 
-bool isPalindrome(const string& s) {
-    int l = 0, r = s.length() - 1;
-    while (l < r) {
-        if (s[l] != s[r]) return false;
-        l++, r--;
-    }
-    return true;
-}
+using namespace  std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
 
-    for (int tc = 1; tc <= 10; ++tc) {
-        int N;
-        cin >> N;
+	int len;
 
-        vector<string> board(8);
-        for (int i = 0; i < 8; ++i)
-            cin >> board[i];
+	for (int i = 1; i <= 10; i++) {
+		cin >> len;
+		char ary[8][8] = { 0 };
 
-        int cnt = 0;
+		// 글자판
+		for (int j = 0; j < 8; j++) {
+			for (int k = 0; k < 8; k++) {
+				cin >> ary[j][k];
+			}
+		}
 
-        // 가로 검사
-        for (int i = 0; i < 8; ++i) {
-            for (int j = 0; j <= 8 - N; ++j) {
-                string row = board[i].substr(j, N);
-                if (isPalindrome(row)) cnt++;
-            }
-        }
+		int cnt = 0;
+		int r_cnt = 0;
 
-        // 세로 검사
-        for (int j = 0; j < 8; ++j) {
-            for (int i = 0; i <= 8 - N; ++i) {
-                string col = "";
-                for (int k = 0; k < N; ++k)
-                    col += board[i + k][j];
-                if (isPalindrome(col)) cnt++;
-            }
-        }
+		// row에서 회문 찾기
+		for (int j = 0; j < 8; j++) {
+			for (int k = 0; k < (8 - len + 1); k++) {
+				int temp_ks = k;
+				int temp_ke = k;
+				r_cnt = 0;
+				while (temp_ks < temp_ke+len-1) {
+					if (ary[j][temp_ks] == ary[j][temp_ke+len-1]) {
+						r_cnt++;
+					}
+					temp_ks++;
+					temp_ke--;
+				}
+				if (r_cnt == len / 2) {
+					cnt++;
+				}
+			}
+		}
 
-        cout << "#" << tc << " " << cnt << "\n";
-    }
+		int c_cnt = 0;
+		// col에서 회문 찾기
+		for (int k = 0; k < 8; k++) {
+			for (int j = 0; j < (8 - len + 1); j++) {
+				int temp_js = j;
+				int temp_je = j;
+				c_cnt = 0;
+				while (temp_js < temp_je + len - 1) {
+					if (ary[temp_js][k] == ary[temp_je + len - 1][k]) {
+						c_cnt++;
+					}
+					temp_js++;
+					temp_je--;
+				}
+				if (c_cnt == len / 2) {
+					cnt++;
+				}
+			}
+		}
 
-    return 0;
+		// 출력
+		cout << "#" << i << " " << cnt << "\n";
+	}
+
+	return 0;
 }
