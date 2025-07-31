9148687dbbd9175e1319c52eeb31097b2b413256
italotelly <81748009+italotelly@users.noreply.github.com>
2025-07-31 22:39:38 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 1,430 ms, Memory: 5,868 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.cpp
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.cpp
@@ -1,87 +1,161 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <vector>
 #include <algorithm>
+
 using namespace std;
 
-int N, ans;
-vector<vector<int>> board;
-vector<pair<int, int>> wormhole[11];
+int T, N, rst;
+int map[100][100];
 
-int dy[4] = { -1, 1, 0, 0 };
-int dx[4] = { 0, 0, -1, 1 };
-
-int reflect[6][4] = {
-	{0,0,0,0},
-	{1,3,0,2},
-	{3,0,1,2},
-	{2,0,3,1},
-	{1,2,3,0},
-	{1,0,3,2}
+struct Point
+{
+	int y;
+	int x;
 };
 
-bool inRange(int y, int x) {
-	return (y >= 0 && x >= 0 && y < N && x < N);
-}
+// 시작점
+Point sp;
 
-pair<int, int> warp(int y, int x) {
-	int num = board[y][x];
-	auto &v = wormhole[num];
-	return (v[0] == make_pair(y, x)) ? v[1] : v[0];
-}
+// 웜홀 정보
+Point wormhole[11][2];
 
-int simulate(int sy, int sx, int dir) {
-	int y = sy, x = sx, d = dir, score = 0;
-	while (true) {
-		y += dy[d];
-		x += dx[d];
+int dy[4] = {-1, 1, 0, 0};
+int dx[4] = {0, 0, -1, 1};
 
-		if (!inRange(y, x)) { score++; d ^= 1; continue; }
-		if ((y == sy && x == sx) || board[y][x] == -1) break;
-
-		int val = board[y][x];
-		if (1 <= val && val <= 5) {
-			score++;
-			d = reflect[val][d];
-		}
-		else if (val >= 6) {
-			auto nxt = warp(y, x);
-			y = nxt.first;
-			x = nxt.second;
+void input() {
+	cin >> N;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			cin >> map[i][j];
 		}
 	}
-	return score;
 }
 
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr);
-
-	int T;
-	cin >> T;
-	for (int tc = 1; tc <= T; tc++) {
-		cin >> N;
-		board.assign(N, vector<int>(N, 0));
-		for (int i = 6; i <= 10; i++) wormhole[i].clear();
-
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				cin >> board[i][j];
-				if (board[i][j] >= 6)
-					wormhole[board[i][j]].push_back({ i,j });
-			}
+// wormhole 초기화
+void initWormhole() {
+	for (int i = 0; i < 11; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			wormhole[i][j].y = -1;
+			wormhole[i][j].x = -1;
 		}
+	}
+}
 
-		ans = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < N; j++) {
-				if (board[i][j] == 0) {
-					for (int d = 0; d < 4; d++) {
-						ans = max(ans, simulate(i, j, d));
-					}
+// 웜홀 찾기
+void findWormhole() {
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (map[i][j] >= 6 && map[i][j] <= 10) {
+				int wormholeNum = map[i][j];
+				if (wormhole[wormholeNum][0].y == -1) {
+					wormhole[wormholeNum][0].y = i;
+					wormhole[wormholeNum][0].x = j;
+				}
+				else {
+					wormhole[wormholeNum][1].y = i;
+					wormhole[wormholeNum][1].x = j;
 				}
 			}
 		}
-		cout << "#" << tc << " " << ans << "\n";
 	}
-	return 0;
+}
+
+// 방향 찾는 함수
+int findDirection(int num, int d) {
+	int directions[5][4] = {
+		{1, 3, 0, 2},
+		{3, 0, 1, 2},
+		{2, 0, 3, 1},
+		{1, 2, 3, 0},
+		{1, 0, 3, 2} };
+
+	return directions[num - 1][d];
+
+}
+
+void simulation(Point cp, int d) {
+	int cnt = 0;
+	while (true) {
+		cp.y += dy[d];
+		cp.x += dx[d];
+
+		// 벽
+		if (cp.y < 0 || cp.y >= N || cp.x < 0 || cp.x >= N)
+		{
+			cnt++;
+			d = findDirection(5, d);
+			continue;
+		}
+
+		// 시작점, 또는 블랙홀
+		if (cp.y == sp.y && cp.x == sp.x || map[cp.y][cp.x] == -1)
+		{
+			rst = max(cnt, rst);
+			return;
+		}
+
+		// 빈 공간
+		if (map[cp.y][cp.x] == 0)
+			continue;
+
+		// 블럭
+		if (map[cp.y][cp.x] >= 1 && map[cp.y][cp.x] <= 5)
+		{
+			cnt++;
+			d = findDirection(map[cp.y][cp.x], d);
+		}
+
+		// 웜홀
+		if (map[cp.y][cp.x] >= 6 && map[cp.y][cp.x] <= 10)
+		{
+			int targetWormholeNum = map[cp.y][cp.x];
+			if (wormhole[targetWormholeNum][0].y == cp.y && wormhole[targetWormholeNum][0].x == cp.x)
+			{
+				cp.y = wormhole[targetWormholeNum][1].y;
+				cp.x = wormhole[targetWormholeNum][1].x;
+			}
+			else
+			{
+				cp.y = wormhole[targetWormholeNum][0].y;
+				cp.x = wormhole[targetWormholeNum][0].x;
+			}
+		}
+
+	}
+}
+
+void solve() {
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			for (int d = 0; d < 4; d++)
+			{
+				if (map[i][j] == 0) {
+					sp.y = i;
+					sp.x = j;
+					simulation(sp, d);
+				}
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++)
+	{
+		input();
+		rst = 0;
+		initWormhole();
+		findWormhole();
+		solve();
+		cout << "#" << tc << " " << rst << "\n";
+	}
 }
