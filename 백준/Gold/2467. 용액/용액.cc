65f10edcaed48bf35885273c1bac819b4832936e
Gwangcheol Ryu <81748009+italotelly@users.noreply.github.com>
2025-11-17 21:00:25 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int N;
+
+int main() {
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	cin >> N;
+	vector<long long> v(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> v[i];
+	}
+
+	int st = 0;
+	int en = N - 1;
+	long long sum = 0;
+	long long min_num = abs(v[st] + v[en]);
+	int min_st = st;
+	int min_en = en;
+
+	while (st < en) {
+		sum = v[st] + v[en];
+		if (abs(min_num) > abs(sum)) {
+			min_num = sum;
+			min_st = st;
+			min_en = en;
+		}
+
+		if (sum == 0) {
+			cout << v[st] << " " << v[en] << "\n";
+			return 0;
+		}
+		else if (sum > 0) {
+			if (abs(sum) > abs(v[st] + v[en - 1])) {
+				en--;
+			}
+			else {
+				en--;
+				st++;
+			}
+		}
+		else if (sum < 0) {
+			if (abs(sum) > abs(v[st + 1] + v[en])) {
+				st++;
+			}
+			else {
+				st++;
+				en--;
+			}
+		}
+	}
+
+	cout << v[min_st] << " " << v[min_en] << "\n";
+
+	return 0;
+}
