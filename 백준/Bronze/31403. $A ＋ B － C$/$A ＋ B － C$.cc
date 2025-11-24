2ae416174bf2850961b7053049b9dc3eff7e05f2
류광철 <81748009+italotelly@users.noreply.github.com>
2025-11-24 23:54:05 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string a, b, c;
+
+	cin >> a >> b >> c;
+
+	cout << stoi(a) + stoi(b) - stoi(c) << "\n";
+	cout << stoi(a + b) - stoi(c) << "\n";
+
+	return 0;
+}
