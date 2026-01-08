7a6f217f06ddb045689203322d2432015e51b261
류광철 <81748009+italotelly@users.noreply.github.com>
2026-01-08 13:15:37 +0900

    [Gold IV] Title: 전화번호 목록, Time: 140 ms, Memory: 6728 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+unordered_map<string, int> m;
+vector<string> v;
+int flag;
+
+void init() {
+	m.clear();
+	v.clear();
+	flag = 0;
+}
+
+int main() {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int t;
+	cin >> t;
+
+	for (int tc = 0; tc < t; tc++)
+	{
+		init();
+		int n;
+		cin >> n;
+		string x;
+		for (int i = 0; i < n; i++)
+		{
+			cin >> x;
+			v.push_back(x);
+			m[x] = 1;
+		}
+		for (int i = 0; i < v.size(); i++)
+		{
+			string s;
+			for (int j = 0; j < v[i].size() - 1; j++)
+			{
+				s += v[i][j];
+				if (m[s]) flag = 1;
+			}
+		}
+
+		if (flag) cout << "NO\n";
+		else cout << "YES\n";
+	}
+
+	return 0;
+}
